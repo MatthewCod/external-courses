531a178da87701ba3af3ddccf39faf8c842a7d79
MatthewCod <mazai228322@gmail.com>
2021-05-30 21:36:50 +0300

    первое и второе задание

--- src/ex15_js-oop/task-01.js
+++ src/ex15_js-oop/task-01.js
@@ -1,52 +1,53 @@
-// function PresentCreator(...candyies) {
-//     this.sortByWeight = function () {
-//         for (let i = 0; i < candyies.length; i++) {
-//             for (let index = 0; index < candyies.length - 1; index++) {
-//                 if (candyies[index].weight > candyies[index + 1].weight) {
-//                     let buff = candyies[index];
-//                     candyies[index] = candyies[index + 1];
-//                     candyies[index + 1] = buff;
-//                 }
-//             }
-//         }
-//         return console.log(candyies);
-//     },
-//         this.searchByName = function (searchQuery) {
-//             return console.log(candyies.find(item => item.name == searchQuery));
-//         }
-// }
+function PresentCreator(...arrayCandy) {
+    let candyies = arrayCandy;
+    this.sortByWeight = function () {
+        for (let i = 0; i < candyies.length; i++) {
+            for (let index = 0; index < candyies.length - 1; index++) {
+                if (candyies[index].weight > candyies[index + 1].weight) {
+                    let buff = candyies[index];
+                    candyies[index] = candyies[index + 1];
+                    candyies[index + 1] = buff;
+                }
+            }
+        }
+        return console.log(candyies);
+    }
+        this.searchByName = function (searchQuery) {
+            return console.log(candyies.find(item => item.name === searchQuery));
+        }
+}
 
-// let candy = {
-//     taste: "sweet",
-//     rustle() {
-//         console.log("shshhshshs");
-//     }
-// }
+let candy = {
+    taste: "sweet",
+    rustle() {
+        console.log("shshhshshs");
+    }
+}
 
-// function CandyConstruct(wrapperColor) {
-//     this.wrapperColor = wrapperColor;
-//     this.containsChocolate = "yes";
-// }
+function CandyConstruct(wrapperColor) {
+    this.wrapperColor = wrapperColor;
+    this.containsChocolate = "yes";
+}
 
-// CandyConstruct.prototype = candy;
+CandyConstruct.prototype = candy;
 
-// const redCandy = new CandyConstruct("red");
+const redCandy = new CandyConstruct("red");
 
-// function RedCandyConstruct(name, weight) {
-//     this.name = name;
-//     this.weight = weight;
-// }
+function RedCandyConstruct(name, weight) {
+    this.name = name;
+    this.weight = weight;
+}
 
-// RedCandyConstruct.prototype = redCandy;
+RedCandyConstruct.prototype = redCandy;
 
-// let kitKat = new RedCandyConstruct("kit-Kat", 30);
-// let step = new RedCandyConstruct("step", 25);
-// let thirtyFive = new RedCandyConstruct("thirtyFive", 15);
-// let redOctober = new RedCandyConstruct("redOctober", 10);
-// let redSweet = new RedCandyConstruct("redSweet", 50);
+let kitKat = new RedCandyConstruct("kit-Kat", 30);
+let step = new RedCandyConstruct("step", 25);
+let thirtyFive = new RedCandyConstruct("thirtyFive", 15);
+let redOctober = new RedCandyConstruct("redOctober", 10);
+let redSweet = new RedCandyConstruct("redSweet", 50);
 
-// let present = new PresentCreator(kitKat, step, thirtyFive, redOctober);
+let present = new PresentCreator(kitKat, step, thirtyFive, redOctober);
 
-// present.sortByWeight();
+present.sortByWeight();
 
-// present.searchByName("step");
+present.searchByName("step");
